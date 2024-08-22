2434819a5661b7da4cbd12080e3e420804efd046
PauloMalavasi <paulomalavasi44@gmail.com>
2024-08-21 22:26:33 -0300

    Add files via upload

---
+++ calculadora c.  media.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+double soma(double n1,double n2);
+double sub(double n1,double n2);
+double mult(double n1,double n2);
+double div(double n1,double n2);
+double med(double n1,double n2);
+int main (){
+	double valor1, valor2;
+	char op;
+	printf("Escolha uma operacao:\n+ adicao\n- subtracao\n/ divsao\n* multiplicacao\nM a media da soma de 2 numeros\n");
+	scanf("%c",&op);
+	printf("Digite o primeiro numero: ");
+	scanf("%lf",&valor1);
+	printf("Digite o segundo numero: ");
+	scanf("%lf",&valor2);
+	if(op == '+'){
+		printf(" soma é: %.1f",soma(valor1,valor2));
+	}else if(op == '-'){
+		printf("A subtracacao é: %.1f",sub(valor1,valor2));
+	}else if(op == '/'){
+		printf("A divisao é: %.1f",div(valor1,valor2));
+	}else if(op == '*'){
+		printf("A multiplicacao é: %.1f",mult(valor1,valor2));
+	}else if(op == 'M'){
+		printf("A media é: %.1f",med(valor1,valor2));
+	}else{
+		printf("A operaçao e invalida!\n");
+	}
+	return 0;
+}  
+double sub(double n1,double n2){
+	 
+	return n1 - n2;
+}
+double soma(double n1,double n2){
+	
+	return n1 + n2;
+	
+}
+double mult(double n1,double n2){
+	
+	  return n1 * n2;
+}
+double div(double n1,double n2){
+	
+	  return n1 / n2;
+}
+double med(double n1,double n2){
+	
+	  return (n1 + n2)/2;
+}
